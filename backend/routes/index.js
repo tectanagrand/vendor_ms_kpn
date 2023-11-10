7c33f99c95beb24f24c1efcc76ba7c0b7b9dd0cc
tectanagrand <rtektano@gmail.com>
2023-11-10 09:07:20 +0700

    Refactor : add routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,6 +4,7 @@ const Ticket = require("./TicketRoute");
 const Vendor = require("./VendorRoute");
 const Master = require("./MasterRoute");
 const Email = require("./EmailRoute");
+const Reqstat = require("./ReqstatRoute");
 const router = express.Router();
 
 router.use("/api/user", User);
@@ -11,5 +12,6 @@ router.use("/api/ticket", Ticket);
 router.use("/api/vendor", Vendor);
 router.use("/api/master", Master);
 router.use("/api/email", Email);
+router.use("/api/reqstat", Reqstat);
 
 module.exports = router;
