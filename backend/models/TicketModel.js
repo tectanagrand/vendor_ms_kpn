3d679ba92a621416719ff4f7ba55256f2a00867a
tectanagrand <rtektano@gmail.com>
2023-11-10 09:06:30 +0700

    Refactor : move ticket request submission to model

--- backend/models/TicketModel.js
+++ backend/models/TicketModel.js
@@ -3,6 +3,8 @@ const uuid = require("uuidv4");
 const TRANS = require("../config/transaction");
 const crud = require("../helper/crudquery");
 const jwt = require("jsonwebtoken");
+const Vendor = require("../models/VendorModel");
+const Emailer = require("../models/EmailModel");
 
 const Ticket = {
     async showAll() {
@@ -75,15 +77,11 @@ const Ticket = {
         const f_today = today.toLocaleDateString();
         until.setDate(today.getDate() + 3);
         const f_until = until.toLocaleDateString();
-        const ticketid = await db.query(
-            "SELECT id FROM ticket order by id desc"
-        );
+        const ticketid = await db.query("SELECT nextval('ticket_id_seq')");
         const ven_id = uuid.uuid();
         const token = uuid.uuid();
         // console.log(ticketid.rows);
-        const latestnum =
-            Number(ticketid.rows.length != 0 ? ticketid.rows[0].id : 0) + 1;
-
+        const latestnum = ticketid.rows[0].nextval;
         const ticketNumber =
             "VMS-" + year + month + String(latestnum).padStart(4, "0");
 
@@ -173,16 +171,18 @@ const Ticket = {
         }
     },
 
-    async rejectTicket(ticket_id, remarks) {
+    async rejectTicket(ticket_id, remarks, role) {
         const client = db;
         try {
             await client.query(TRANS.BEGIN);
             const ticketq =
-                await client.query(`SELECT tic.token as ticket_id, tic.cur_pos, proc.department as proc, mdm.department as mdm, v.is_tender, v.name_1  from ticket tic
+                await client.query(`SELECT tic.token as ticket_id, tic.ticket_id as ticket_num, tic.cur_pos, proc.department as proc, mdm.department as mdm, v.is_tender, v.name_1  from ticket tic
                         left join (select user_id, department from mst_user) proc on proc.user_id = tic.proc_id
                         left join (select user_id, department from mst_user) mdm on mdm.user_id = tic.mdm_id
                         left join vendor v on tic.ven_id = v.ven_id 
                         where tic.token = '${ticket_id}'`);
+            const targets = await this.ticketTarget(ticket_id);
+            const dataTrg = targets.data;
             const ticket = ticketq.rows[0];
             const session = ticket.cur_pos;
             let reject_by;
@@ -213,6 +213,10 @@ const Ticket = {
                                 where token = '${ticket.ticket_id}'
                                 returning ticket_id`;
             const upTick = await client.query(q);
+            await Emailer.toReject(remarks, ticket.name_1, dataTrg.proc_email, [
+                dataTrg.mgr_pr_email,
+                dataTrg.mgr_md_email,
+            ]);
             await client.query(TRANS.COMMIT);
             return [upTick.rows[0].ticket_id, reject_by, ticket.name_1];
         } catch (err) {
@@ -250,18 +254,57 @@ const Ticket = {
         }
     },
 
-    // async refreshTicketToken (params) {
-    //     try {
-    //         const q = `select cur_pos from ticket where ticket_id = '${params.ticketnum}'` ;
-    //         const curpos = await db.query(q) ;
-    //         if(curpos != 'VENDOR') {
-    //             return {status : true, message : 'Form already been submitted'}
-    //         }
-    //         const newToken =
-    //     } catch (error) {
-
-    //     }
-    // }
+    async submitVendor({
+        ven_detail,
+        ven_banks,
+        ven_files,
+        ticket_id,
+        remarks,
+        is_draft,
+        role,
+    }) {
+        //set vendor detail
+        const client = db;
+        try {
+            await client.query(TRANS.BEGIN);
+            const client1 = await Vendor.setDetailVen(ven_detail, client);
+            const client2 = await Vendor.setBank(ven_banks, client1);
+            const client3 = await Vendor.setFile(ven_files, client2);
+            console.log(client3);
+            const ticket = await this.submitTicket(
+                { ticket_id: ticket_id, remarks: remarks },
+                client3
+            );
+            const targets = await this.ticketTarget(ticket_id);
+            const dataTrg = targets.data;
+            const res_tnum = ticket.rows[0].ticket_id;
+            if (!is_draft && role === "PROC") {
+                await Emailer.toRequest(
+                    ven_detail.ticket_num,
+                    dataTrg.proc_fname,
+                    dataTrg.proc_email,
+                    [dataTrg.mgr_pr_email]
+                );
+            } else if (!is_draft && role === "MDM") {
+                await Emailer.toApprove(
+                    ven_detail.ven_code,
+                    ven_detail.name_1,
+                    dataTrg.proc_email,
+                    [
+                        dataTrg.mgr_pr_email,
+                        dataTrg.mgr_md_email,
+                        dataTrg.mdm_email,
+                    ]
+                );
+            }
+            await client.query(TRANS.COMMIT);
+            return res_tnum;
+        } catch (error) {
+            await client.query(TRANS.ROLLBACK);
+            console.error(error);
+            throw error;
+        }
+    },
 };
 
 module.exports = Ticket;
