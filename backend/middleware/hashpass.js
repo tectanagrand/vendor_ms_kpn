727e2e06aadc6a0226fba2d78f6a4917d6c878e6
tectanagrand <rtektano@gmail.com>
2023-11-30 08:29:02 +0700

    clean up

--- backend/middleware/hashpass.js
+++ backend/middleware/hashpass.js
@@ -16,6 +16,7 @@ async function hashPassword(user) {
 
 async function validatePassword({ password, hashed }) {
     return bcrypt.compareSync(password, hashed);
+    // return false;
 }
 
 module.exports = { hashPassword, validatePassword };
