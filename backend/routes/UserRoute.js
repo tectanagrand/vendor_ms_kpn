d97a1bb90f9762bcab80d2b3353ed7a24693efa7
tectanagrand <rtektano@gmail.com>
2023-12-18 11:02:28 +0700

    refactor : add auth token

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,19 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/UserController");
+const AuthToken = require("../middleware/tokenmanager");
 
 router.get("/", controller.showAll);
 router.post("/login", controller.loginUser);
 router.get("/refresh", controller.refreshToken);
-router.post("/authorization", controller.getAuthorization);
+router.post(
+    "/authorization",
+    AuthToken.authSession,
+    controller.getAuthorization
+);
 router.get("/check", controller.check);
 router.get("/lssecmtx", controller.showExistSecGrp);
 router.get("/roles", controller.showRole);
 router.get("/mgrs", controller.showManagers);
 router.get("/show", controller.showUserData);
 router.post("/secmtx", controller.showSecurityGroup);
-router.post("/secmtx/submit", controller.submitSecurityGroup);
-router.post("/submit", controller.createNewUser);
+router.post(
+    "/secmtx/submit",
+    AuthToken.authSession,
+    controller.submitSecurityGroup
+);
+router.post("/submit", AuthToken.authSession, controller.createNewUser);
 // router.put("/:id/edit", controller.editUser);
 // router.get("/:id/edit", controller.getUserById);
 // router.delete("/:id", controller.deleteUserId);
