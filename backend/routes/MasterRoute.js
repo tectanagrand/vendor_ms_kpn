d6b284a6ea53e98a2ed4501a10fd27f3edee399a
tectanagrand <rtektano@gmail.com>
2023-12-18 11:00:09 +0700

    add : routes bank ssr, crud bank, file download w/o authorization (temp)

--- backend/routes/MasterRoute.js
+++ backend/routes/MasterRoute.js
@@ -7,7 +7,13 @@ router.post("/country", controller.getCountry);
 router.post("/city", controller.getCity);
 router.get("/curr", controller.getCurrency);
 router.get("/bank", controller.getBank);
+router.get("/banksap", controller.getBankSAP);
 router.get("/company", controller.getCompany);
-router.get("/file/:filename", Auth.authSession, controller.downloadFile);
+router.get("/file/:filename", controller.downloadFile);
+router.get("/genqr", controller.genQrcode);
+router.get("/payterm", controller.getPayterm);
+router.post("/ssrbank", controller.getBankSSR);
+router.post("/addbank", controller.insertBank);
+router.post("/deletebank", controller.deleteBank);
 
 module.exports = router;
