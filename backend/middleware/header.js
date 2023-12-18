e65b929e3d082dbcae1f59972caef4928a86afca
tectanagrand <rtektano@gmail.com>
2023-12-18 10:51:10 +0700

    refactor : header cors policy

--- backend/middleware/header.js
+++ backend/middleware/header.js
@@ -1,10 +1,15 @@
 const allowCrossDomain = (req, res, next) => {
-    const allowedOrigins = [`http://localhost:3003`, `http://localhost:3000`];
+    const allowedOrigins = [
+        "http://172.30.61.136:3000",
+        "http://172.29.0.1:3000",
+        `http://localhost:3003`,
+        `http://localhost:3000`,
+    ];
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
         res.header(`Access-Control-Allow-Origin`, origin);
     }
-    res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
+    res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE,PATCH`);
     res.header(`Access-Control-Allow-Headers`, `Content-Type`);
     next();
 };
