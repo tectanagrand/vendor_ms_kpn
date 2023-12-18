932c0178110a30141274df43d0c52a8a43d35eb9
tectanagrand <rtektano@gmail.com>
2023-12-18 10:38:07 +0700

    refactor add https on server

--- server.js
+++ server.js
@@ -2,19 +2,24 @@ const express = require("express");
 const header = require("./backend/middleware/header");
 const app = express();
 const dotenv = require("dotenv").config();
+const os = require("os");
+const https = require("https");
 const path = require("path");
 const routers = require("./backend/routes");
 const port = process.env.PORT;
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const fs = require("fs");
+
 const whitelist = [
-    "http://172.30.60.86:3000",
-    "http://localhost:3003/",
-    "http://127.0.0.1:3003/",
-    "http://localhost:3000/",
-    "http://vms.com:3000/",
-    "http://localhost:5000/",
+    "http://172.30.60.50:3000",
+    "http://172.29.0.1:3000",
+    "http://localhost:3000",
 ];
+const servOption = {
+    cert: fs.readFileSync("./ssl/certificate.crt"),
+    key: fs.readFileSync("./ssl/private-key.pem"),
+};
 const corsOption = {
     // origin: [
     //     "http://172.30.60.86:3000",
@@ -29,7 +34,7 @@ const corsOption = {
             callback(null, false);
         }
     },
-    methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD", "DELETE"],
+    methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD", "DELETE", "PATCH"],
     credentials: true,
     exposedHeaders: ["set-cookie"],
 };
@@ -37,7 +42,11 @@ const corsOption = {
 // app.use(header);
 app.set("view engine", "ejs");
 app.use(header);
-app.set("views", path.join(__dirname, "\\backend\\views\\pages"));
+if (os.platform() === "linux") {
+    app.set("views", path.join(__dirname, "/backend/views/pages"));
+} else {
+    app.set("views", path.join(__dirname, "\\backend\\views\\pages"));
+}
 app.use(cors(corsOption));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -48,6 +57,10 @@ app.use(express.static(path.join(__dirname, "public/build")));
 app.get("/*$", (req, res) => {
     res.sendFile(path.join(__dirname, "public/build", "index.html"));
 });
-app.listen(port, "0.0.0.0", () => {
+
+const server = https.createServer(servOption, app).listen(port, () => {
     console.log(`App running on ${port}`);
 });
+// app.listen(port, "0.0.0.0", () => {
+//     console.log(`App running on ${port}`);
+// });
