3bb8611492e55d08f32d4f2425828ea3553e5e0c
tectanagrand <rtektano@gmail.com>
2023-12-18 11:00:40 +0700

    refactor : stat request w/ auth token

--- backend/routes/ReqstatRoute.js
+++ backend/routes/ReqstatRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const Reqstat = require("../controllers/ReqstatController");
+const AuthToken = require("../middleware/tokenmanager");
 
 const router = express.Router();
 
 router.post("/new", Reqstat.request);
-router.post("/process", Reqstat.processReq);
+router.post("/process", AuthToken.authSession, Reqstat.processReq);
 router.get("/show", Reqstat.showAll);
 
 module.exports = router;
