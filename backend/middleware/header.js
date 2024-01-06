159e367023cbe02e201c9e8c57f8654ce6507e1d
tectanagrand <rtektano@gmail.com>
2024-01-06 18:47:16 +0700

    fix : header

--- backend/middleware/header.js
+++ backend/middleware/header.js
@@ -3,7 +3,7 @@ const allowCrossDomain = (req, res, next) => {
         "http://172.30.61.136:3000",
         "http://172.29.0.1:3000",
         `http://localhost:3003`,
-        `http://localhost:3000`,
+        `https://localhost:3000`,
     ];
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
