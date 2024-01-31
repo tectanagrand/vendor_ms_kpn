2f6c84a12585b50201cc80de4b9a28d9291a6698
tectanagrand <rtektano@gmail.com>
2024-01-31 09:27:57 +0700

    feat : get info ven

--- backend/routes/VendorRoute.js
+++ backend/routes/VendorRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const route = express.Router();
 const controller = require("../controllers/VendorController");
+const Auth = require("../middleware/tokenmanager");
 
 route.get("/", controller.showAll);
 route.post("/add/:id", controller.setVenDetail);
@@ -9,5 +10,6 @@ route.get("/file/:id", controller.getFile);
 route.get("/bank/:id", controller.getBank);
 route.delete("/file", controller.deleteTempId);
 route.get("/checkven", controller.checkNameisExist);
+route.get("/infoven/", Auth.authSession, controller.getInfoVen);
 
 module.exports = route;
