0d407a0077e6d21d8738d840efb6e85b63652603
tectanagrand <rtektano@gmail.com>
2024-01-31 09:20:34 +0700

    ref : pm2 set env variable toggle

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,6 +6,12 @@ module.exports = {
             watch: true,
             autorestart: true,
             exp_backoff_restart_delay: 100,
+            env: {
+                NODE_ENV: "development",
+            },
+            env_production: {
+                NODE_ENV: "production",
+            },
         },
     ],
 };
