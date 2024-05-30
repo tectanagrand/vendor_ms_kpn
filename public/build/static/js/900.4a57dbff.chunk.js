144e150764a328da7092ca6c119bdb347a00cb90
tectanagrand <rtektano@gmail.com>
2024-05-30 07:30:21 +0700

    chore : remove from changes git

--- public/build/static/js/900.4a57dbff.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkui_vms=self.webpackChunkui_vms||[]).push([[900],{6900:function(e,v,u){var c=u(4836);Object.defineProperty(v,"__esModule",{value:!0}),v.default=void 0;var h=c(u(5649)),s=u(184),a=(0,h.default)((0,s.jsx)("path",{d:"M22 10V6c0-1.11-.9-2-2-2H4c-1.1 0-1.99.89-1.99 2v4c1.1 0 1.99.9 1.99 2s-.89 2-2 2v4c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2v-4c-1.1 0-2-.9-2-2s.9-2 2-2zm-9 7.5h-2v-2h2v2zm0-4.5h-2v-2h2v2zm0-4.5h-2v-2h2v2z"}),"ConfirmationNumber");v.default=a}}]);
-//# sourceMappingURL=900.4a57dbff.chunk.js.map
